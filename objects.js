6155e1f24b6593817a931e730e7480bd31362fce
Wil Cook <wil.cook.is@gmail.com>
2018-10-19 13:14:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,5 @@ var playlist = new Object({
   ParquetCourts: "Human Nature",
   TheBlackKids: "I\'m not going to teach your boyfriend how to dance with you."
 });
+
+function updatePlaylist(obj, key, val)
